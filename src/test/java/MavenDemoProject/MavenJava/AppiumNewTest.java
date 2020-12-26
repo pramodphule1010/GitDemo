4dc32464fd2099e88fb61170b50f3ebf55883db9
pramod <pramodphule107@gmail.com>
2020-12-26 20:35:15 +0530

    first commit

---
+++ src/test/java/MavenDemoProject/MavenJava/AppiumNewTest.java
@@ -0,0 +1,19 @@
+package MavenDemoProject.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumNewTest {
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSAPP");
+		
+	}
+
+}
